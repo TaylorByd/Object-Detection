fd4b2729b99977868fdc7fcb297ef52cec92a8d0
Taylor Boyd <epicdrak@gmail.com>
2024-04-18 15:23:36 -0700

    modified the for loop to get rid of the img_name_element variable.

--- DataCollection/CaptureData/CaptureObjects.py
+++ DataCollection/CaptureData/CaptureObjects.py
@@ -7,27 +7,23 @@ cam = cv2.VideoCapture(0)
 # names of the four unique objects that I will be using for data
 img_names = ["object1", "object2", "object3", "object4"]
 
-# variable used to access the img names
-img_name_element = 0 
-
 # This will create 5 photos for each object when I press the spacebar.
-for i in range(4):
+for img_name in img_names:
     img_counter = 1  
     while (img_counter <= 5):
         ret, frame = cam.read() 
 
         # Display the resulting frame 
-        cv2.imshow('frame', frame) 
+        cv2.imshow('frame', frame)  
 
-        k = cv2.waitKey(1) 
+        k = cv2.waitKey(1)   
         if k%256 == 32: 
-            # The space key was pressed
-            img_name = f"{img_names[img_name_element]}_{img_counter}.png"
-            path = f"./DataCollection/CaptureData/ObjectPhotos/{img_names[img_name_element]}"
-            cv2.imwrite(os.path.join(path, img_name), frame)
-            print("{} written!".format(img_name))
+            # The space key was pressed 
+            img_file_name = f"{img_name}_{img_counter}.png"
+            path = f"./DataCollection/CaptureData/ObjectPhotos/{img_name}"
+            cv2.imwrite(os.path.join(path, img_file_name), frame)
+            print("{} written!".format(img_file_name))
             img_counter += 1
-    img_name_element += 1
 
 # After the loop release the cap object 
 cam.release() 
