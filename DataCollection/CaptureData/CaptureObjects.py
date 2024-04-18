2a625471410dbe56ba2d95a2e158f2c9964381d4
Taylor Boyd <epicdrak@gmail.com>
2024-04-18 15:08:46 -0700

    Created a way to capture multiple photos for 4 distinct objects using the computer vision library.

--- DataCollection/CaptureData/CaptureObjects.py
+++ DataCollection/CaptureData/CaptureObjects.py
@@ -1,1 +1,35 @@
-# Start of the CaptureObjects python file.
+import cv2
+import os
+
+# define a camera object
+cam = cv2.VideoCapture(0) 
+
+# names of the four unique objects that I will be using for data
+img_names = ["object1", "object2", "object3", "object4"]
+
+# variable used to access the img names
+img_name_element = 0 
+
+# This will create 5 photos for each object when I press the spacebar.
+for i in range(4):
+    img_counter = 1  
+    while (img_counter <= 5):
+        ret, frame = cam.read() 
+
+        # Display the resulting frame 
+        cv2.imshow('frame', frame) 
+
+        k = cv2.waitKey(1) 
+        if k%256 == 32: 
+            # The space key was pressed
+            img_name = f"{img_names[img_name_element]}_{img_counter}.png"
+            path = f"./DataCollection/CaptureData/ObjectPhotos/{img_names[img_name_element]}"
+            cv2.imwrite(os.path.join(path, img_name), frame)
+            print("{} written!".format(img_name))
+            img_counter += 1
+    img_name_element += 1
+
+# After the loop release the cap object 
+cam.release() 
+# Destroy all the windows 
+cv2.destroyAllWindows() 
