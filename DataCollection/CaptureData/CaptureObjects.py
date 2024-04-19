414eebc76ced43394e0c31b6f7879391173b02ef
Taylor Boyd <epicdrak@gmail.com>
2024-04-18 21:05:46 -0700

    changed the file format to be a png instead of jpg for better quality photos

--- DataCollection/CaptureData/CaptureObjects.py
+++ DataCollection/CaptureData/CaptureObjects.py
@@ -7,20 +7,23 @@ cam = cv2.VideoCapture(0)
 
 # names of the four unique objects that I will be using for data
 img_names = ["object1", "object2", "object3", "object4"]
-
+img_path = os.path.join('DataCollection', 'CaptureData', 'ObjectPhotos')
+ 
 # This will create 5 photos for each object when I press the spacebar.
 for img_name in img_names:
+    if not os.path.exists(os.path.join(img_path, img_name)):
+        os.mkdir(os.path.join(img_path, img_name))
     img_counter = 1  
     while (img_counter <= 5):
         ret, frame = cam.read() 
 
         # Display the resulting frame 
         cv2.imshow('frame', frame)  
-
+  
         k = cv2.waitKey(1)   
         if k%256 == 32:
             # The space key was pressed 
-            img_file_name = img_name + '.' + '{}.jpg'.format(str(uuid.uuid1()))
+            img_file_name = img_name + '.' + '{}.png'.format(str(uuid.uuid1()))
             path = f"./DataCollection/CaptureData/ObjectPhotos/{img_name}"
             cv2.imwrite(os.path.join(path, img_file_name), frame)
             print(f"{img_counter} {img_file_name} written!")
